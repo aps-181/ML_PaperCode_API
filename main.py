219a32b13dbd1ba74a5af4c8a67dc0332a1787b1
aps <aravindpsabu@gmail.com>
2023-05-25 08:50:49 +0530

    second commit

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from guesslang import Guess
+import json
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    return "home"
+# @app.route("/")
+# def home():
+#     return "home"
 
 
-@app.route('/get-code-language')
+@app.route('/')
 def get_code_language():
     code = request.args.get("code")
 
     guess = Guess()
-
-    name = guess.language_name(code)
+    print(code)
+    name = guess.language_name(str(code))
 
     print(name)
-    return name
+    return jsonify({"language": name})
 
+
+@app.route('/getLanguage', methods=["POST"])
+def getLanguage():
+    data = request.get_json()
+    print(data['code'])
+    guess = Guess()
+    code = data['code']
+    name = guess.language_name(str(code))
+
+    print(name)
+    return jsonify({"language": name})
+
+
+port = 5000
 
 if __name__ == "__main__":
+    # app.run(host="0.0.0.0", port=port)
     app.run(debug=True)
